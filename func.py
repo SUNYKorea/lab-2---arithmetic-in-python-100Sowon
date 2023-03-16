befd9e0dc2065b194bad7a13c72048ce63f35023
Sowon Baek <sowon.baek@stonybrook.edu>
2023-03-16 23:53:57 +0900

    Final final done

--- func.py
+++ func.py
@@ -33,8 +33,8 @@ a = 1
 b = -3
 c = 1
 
-x1 = div(add(-b, exp(sub(exp(b,2), multi(multi(-4, a),c)), 0.5)), multi(2,a))# TODO: write a code to compute the first root of the quadratic equation
-x2 = div(sub(-b, exp(sub(exp(b,2), multi(multi(-4, a),c)), 0.5)), multi(2,a)) # TODO: then do the same for the second root
+x1 = div(add(neg(b),sqrt(sub(exp(b, 2), multi(multi(4,a),c)))),multi(2,a)) # TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b),sqrt(sub(exp(b, 2), multi(multi(4,a),c)))),multi(2,a)) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
